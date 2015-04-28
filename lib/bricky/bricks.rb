5fd267d0ce994a5e9d716850c43758a360b4fcf4
andrerocker <andre.souza@gmail.com>
2015-04-27 21:18:26 -0300

    bricky: add bundle :p

--- lib/bricky/bricks.rb
+++ lib/bricky/bricks.rb
@@ -2,6 +2,7 @@ require "byebug"
 require "bricky/bricks/helper"
 
 require "bricky/bricks/ruby"
+require "bricky/bricks/bundle"
 require "bricky/bricks/debian"
 require "bricky/bricks/mounts"
 
@@ -16,8 +17,9 @@ module Bricky
     end
 
     def resolve_and_initialize(name, config)
-      return Bricky::Bricks::Mounts.new(config) if name.eql? "mounts"
+      return Bricky::Bricks::Bundle.new(config) if name.eql? "bundle"
       return Bricky::Bricks::Debian.new(config) if name.eql? "debian"
+      return Bricky::Bricks::Mounts.new(config) if name.eql? "mounts"
     end
   end
 end
---
+++ lib/bricky/bricks/bundle.rb
@@ -0,0 +1,22 @@
+module Bricky
+  module Bricks
+    class Bundle < Helper
+      def arguments
+        scripts_path = "#{bricks_path}/bundle"
+        results = ["-v #{scripts_path}:/bricks/bundle"]
+        results.push(cached) if config["cache"]
+        results
+      end
+  
+      def entrypoint
+        "/bricks/bundle/builder"
+      end
+
+      private
+        def cached
+          digest = Digest::MD5.file('Gemfile').hexdigest
+          ["-v /tmp/bricky/cache/#{digest}:/opt/build/source/vendor/bundle"]
+        end
+    end
+  end
+end
