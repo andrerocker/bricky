d97112c25311bf3d1cc2f8dec5018519572a2168
andrerocker <andre.souza@gmail.com>
2015-05-15 21:54:28 -0300

    bricks: add support to 'ignore' on mounts brick

--- lib/bricky/bricks/mounts.rb
+++ lib/bricky/bricks/mounts.rb
@@ -2,17 +2,28 @@ module Bricky
   module Bricks
     class Mounts < Base
       def arguments
-        scripts_path = "#{bricks_path}/mounts"
-        command = "-v #{scripts_path}:/bricks/mounts"
-
-        config.inject([command]) do |acc, mount|
-          acc << "-v #{File.expand_path(mount.last)}:/bricks/mounts/volumes/#{mount.first}"
+        result = config.inject([commands_volume]) do |acc, mount|
+          acc << mount_point(mount.last, mount.first) if not mount.first.eql? "ignore"
+          acc
         end
       end
   
       def entrypoint
         "/bricks/mounts/builder"
       end
+
+      def environments
+        ["-e BRICKS_MOUNTS_IGNORE='#{config.fetch('ignore', []).join(',')}'"]
+      end
+
+      private
+        def commands_volume
+          "-v #{bricks_path}/mounts:/bricks/mounts"
+        end
+
+        def mount_point(path, name)
+          "-v #{File.expand_path(path)}:/bricks/mounts/volumes/#{name}"
+        end
     end
   end
 end
