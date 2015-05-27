11fdbf3da1d87e608610157d4ec2a5a4b941f673
Eric Camalionte <eric.camalionte@locaweb.com.br>
2015-05-27 17:04:56 -0300

    remove private when we have not defined instance methods

--- lib/bricky/setup.rb
+++ lib/bricky/setup.rb
@@ -1,21 +1,20 @@
-require "bricky/image"
-require "bricky/config"
+require 'bricky/image'
+require 'bricky/config'
 
 module Bricky
   class Setup
     def self.start
       from_brickyfile
-    end    
+    end
 
-    private
-      def self.from_brickyfile
-        if File.exists?(configuration)
-          eval(open("#{Dir.pwd}/Brickyfile").read) 
-        end
+    def self.from_brickyfile
+      if File.exists?(configuration)
+        eval(open("#{Dir.pwd}/Brickyfile").read)
       end
+    end
 
-      def self.configuration
-        "#{Dir.pwd}/Brickyfile"
-      end
+    def self.configuration
+      "#{Dir.pwd}/Brickyfile"
+    end
   end
 end
