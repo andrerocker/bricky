93dce97143233b6c682fca9ea933dc6e4d9cba0c
Eric Camalionte <eric.camalionte@locaweb.com.br>
2015-05-27 17:19:09 -0300

    remove double-quoted strings and inconsistent indentation

--- lib/bricky/requirements.rb
+++ lib/bricky/requirements.rb
@@ -2,22 +2,23 @@ module Bricky
   class Requirements
     def check_and_execute
       if pending_requirements
-        print "Make sure you have the following requirements: " 
+        print 'Make sure you have the following requirements: '
         puts requirements.join(',').colorize(:red)
       else
-        yield          
+        yield
       end
     end
 
     private
-      def requirements
-        ["docker"]
+
+    def requirements
+      ['docker']
+    end
+
+    def pending_requirements
+      requirements.detect do |command|
+        not system("which #{command} > /dev/null 2>&1")
       end
-  
-      def pending_requirements
-        requirements.detect do |command|
-          not system("which #{command} > /dev/null 2>&1")
-        end
-      end
+    end
   end
 end
