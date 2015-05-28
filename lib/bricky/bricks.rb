d649f54c2556b3277643aded1f09ebbe0d0e70e5
Eric Camalionte <eric.camalionte@locaweb.com.br>
2015-05-28 16:41:45 -0300

    adjust to rubocop style - bricks

--- lib/bricky/bricks.rb
+++ lib/bricky/bricks.rb
@@ -1,30 +1,31 @@
-require "bricky/bricks/base"
+require 'bricky/bricks/base'
 
-require "bricky/bricks/helper"
-require "bricky/bricks/ruby"
-require "bricky/bricks/bundle"
-require "bricky/bricks/debian"
-require "bricky/bricks/mounts"
+require 'bricky/bricks/helper'
+require 'bricky/bricks/ruby'
+require 'bricky/bricks/bundle'
+require 'bricky/bricks/debian'
+require 'bricky/bricks/mounts'
 
 module Bricky
   module Bricks
-    extend self
+    module_function
 
     def resolve
       default_bricks + Bricky.config.bricks.collect do |name, config|
-       resolve_and_initialize(name, config)
+        resolve_and_initialize(name, config)
       end.uniq
     end
 
     def resolve_and_initialize(name, config)
-      return Bricky::Bricks::Bundle.new(config) if name.eql? "bundle"
-      return Bricky::Bricks::Debian.new(config) if name.eql? "debian"
-      return Bricky::Bricks::Mounts.new(config) if name.eql? "mounts"
+      return Bricky::Bricks::Bundle.new(config) if name.eql? 'bundle'
+      return Bricky::Bricks::Debian.new(config) if name.eql? 'debian'
+      return Bricky::Bricks::Mounts.new(config) if name.eql? 'mounts'
     end
 
     private
-      def default_bricks
-        [Bricky::Bricks::Helper.new({})]
-      end
+
+    def default_bricks
+      [Bricky::Bricks::Helper.new({})]
+    end
   end
 end
