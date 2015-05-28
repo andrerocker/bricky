40562b5888cf4bcc038c37e1e8754c798b0567b8
Eric Camalionte <eric.camalionte@locaweb.com.br>
2015-05-28 16:43:25 -0300

    adjust to rubocop style - bricks mounts

--- lib/bricky/bricks/mounts.rb
+++ lib/bricky/bricks/mounts.rb
@@ -3,13 +3,13 @@ module Bricky
     class Mounts < Base
       def arguments
         result = config.inject([commands_volume]) do |acc, mount|
-          acc << mount_point(mount.last, mount.first) if not mount.first.eql? "ignore"
+          acc << mount_point(mount.last, mount.first) if not mount.first.eql? 'ignore'
           acc
         end
       end
-  
+
       def entrypoint
-        "/bricks/mounts/builder"
+        '/bricks/mounts/builder'
       end
 
       def environments
@@ -17,13 +17,14 @@ module Bricky
       end
 
       private
-        def commands_volume
-          "-v #{bricks_path}/mounts:/bricks/mounts"
-        end
 
-        def mount_point(path, name)
-          "-v #{File.expand_path(path)}:/bricks/mounts/volumes/#{name}"
-        end
+      def commands_volume
+        "-v #{bricks_path}/mounts:/bricks/mounts"
+      end
+
+      def mount_point(path, name)
+        "-v #{File.expand_path(path)}:/bricks/mounts/volumes/#{name}"
+      end
     end
   end
 end
