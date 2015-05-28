0f1746acf8b5bc7109185a311da18e6e49e14889
Eric Camalionte <eric.camalionte@locaweb.com.br>
2015-05-28 16:44:02 -0300

    adjust to rubocop style - command

--- lib/bricky/command.rb
+++ lib/bricky/command.rb
@@ -1,30 +1,30 @@
-require "thor"
-require "ostruct"
-require "colorize"
+require 'thor'
+require 'ostruct'
+require 'colorize'
 
-require "bricky/image"
-require "bricky/requirements"
-require "bricky/commands/base"
-require "bricky/commands/install"
-require "bricky/commands/builder"
-require "bricky/commands/bootstrap"
+require 'bricky/image'
+require 'bricky/requirements'
+require 'bricky/commands/base'
+require 'bricky/commands/install'
+require 'bricky/commands/builder'
+require 'bricky/commands/bootstrap'
 
 module Bricky
   class Command < Thor
-    desc :install, "install configuration files"
+    desc :install, 'install configuration files'
     def install
       dispatch(:install)
     end
 
-    desc :bootstrap, "bootstrap builder images"
+    desc :bootstrap, 'bootstrap builder images'
     def bootstrap
       requirements.check_and_execute do
         dispatch(:bootstrap)
       end
     end
 
-    desc :builder, "build project"
-    method_option :shell, :type => :boolean, :aliases => "-s"
+    desc :builder, 'build project'
+    method_option :shell, type: :boolean, aliases: '-s'
     def builder
       requirements.check_and_execute do
         dispatch(:builder)
@@ -32,14 +32,15 @@ module Bricky
     end
 
     private
-      def requirements
-        Bricky::Requirements.new
-      end
 
-      def dispatch(name)
-        clazz = Bricky::Commands.const_get("#{name.to_s.capitalize}")
-        command = clazz.new(options)
-        command.execute
-      end
+    def requirements
+      Bricky::Requirements.new
+    end
+
+    def dispatch(name)
+      clazz = Bricky::Commands.const_get("#{name.to_s.capitalize}")
+      command = clazz.new(options)
+      command.execute
+    end
   end
 end
