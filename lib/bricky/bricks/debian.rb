1ec4c18f374c4abecdbb81fffa20981e9d395afd
andrerocker <andre.souza@gmail.com>
2015-07-25 19:17:17 -0300

    debian: fixing parametrized path

--- lib/bricky/bricks/debian.rb
+++ lib/bricky/bricks/debian.rb
@@ -2,7 +2,7 @@ module Bricky
   module Bricks
     class Debian < Base
       def arguments
-        builded_path = ENV.fetch('PACKAGE_OUTPUT_PATH', File.expand_path(config["build"]))
+        builded_path = File.expand_path(ENV.fetch('PACKAGE_OUTPUT_PATH', config["build"]))
         scripts_path = "#{bricks_path}/debian"
 
         %W(-v #{builded_path}:/builded
