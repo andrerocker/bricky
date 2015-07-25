ada45f0d35332239079614eefd401310d54a7d44
andrerocker <andre.souza@gmail.com>
2015-07-25 19:05:13 -0300

    debian: now is possible set a env to define output

--- lib/bricky/bricks/debian.rb
+++ lib/bricky/bricks/debian.rb
@@ -2,7 +2,7 @@ module Bricky
   module Bricks
     class Debian < Base
       def arguments
-        builded_path = File.expand_path(config["build"])
+        builded_path = ENV.fetch('PACKAGE_OUTPUT_PATH', File.expand_path(config["build"]))
         scripts_path = "#{bricks_path}/debian"
 
         %W(-v #{builded_path}:/builded
