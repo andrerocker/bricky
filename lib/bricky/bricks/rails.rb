2b02d88501c97bd959c4a72c355f0890c8d2df67
andrerocker <andre.souza@gmail.com>
2015-08-06 02:18:06 -0300

    rails: assets cache draft

--- lib/bricky/bricks/rails.rb
+++ lib/bricky/bricks/rails.rb
@@ -2,7 +2,7 @@ module Bricky
   module Bricks
     class Rails < Base
       def arguments
-        ["-v #{bricks_path}/rails:/bricks/rails"]
+        ["-v #{bricks_path}/rails:/bricks/rails"].push(assets_cached)
       end
   
       def entrypoint
@@ -10,7 +10,21 @@ module Bricky
       end
 
       def environments
-        ["-e BRICKS_RAILS_ASSETS_PRECOMPILE='#{config.fetch('assets_precompile', false)}'"]
+        ["-e BRICKS_RAILS_ASSETS_PRECOMPILE='#{config.fetch('assets_precompile', false)}'",
+         "-e BRICKS_RAILS_ASSETS_PRECOMPILE_CACHED_DIGEST='#{digest}'"]
+      end
+
+      private
+      def assets_cached
+        ["-v #{local_path}:/opt/workspace/source/public/assets"]
+      end
+
+      def local_path
+        "#{Bricky.config.cache_path}/rails/assets/#{digest}".tap { |path| FileUtils::mkdir_p(path) }
+      end
+
+      def digest
+        @digest ||= %x(find app/assets -type f -exec md5sum {} \\; | md5sum).split.first
       end
     end
   end
