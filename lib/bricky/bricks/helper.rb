05fa0a677dc3a2ee1fddb022ffd5722c50db27ad
andrerocker <andre.souza@gmail.com>
2015-08-07 14:05:50 -0300

    helpers: add support to SSH_AUTH_SOCKET
    - with this feature is possible to use 'hoster' ssh keys (ssh-agent)

--- lib/bricky/bricks/helper.rb
+++ lib/bricky/bricks/helper.rb
@@ -2,17 +2,23 @@ module Bricky
   module Bricks
     class Helper < Base
       def arguments
-        ["-v #{bricks_path}/helper:/bricks/helper"]
+        ["-v #{bricks_path}/helper:/bricks/helper",
+         "-v #{ssh_auth_socket}:/tmp/ssh-auth.socket"]
       end
 
       def environments
         ["-e BRICKS_LINUX=#{linux}",
-          "-e BRICKS_HOME=/bricks/helper"]
+         "-e BRICKS_HOME=/bricks/helper",
+         "-e SSH_AUTH_SOCK=/tmp/ssh-auth.socket"]
       end
 
       private
         def linux
           RUBY_PLATFORM.include?("linux")
+        end
+
+        def ssh_auth_socket
+          ENV['SSH_AUTH_SOCK']
         end
     end
   end
