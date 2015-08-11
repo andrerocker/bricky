fcc3d8fb81ccaa25cd88d019ac0279c12dc8d86f
andrerocker <andre.souza@gmail.com>
2015-08-10 22:05:25 -0300

    helper: enable ssh agent only o envs with the ENV var

--- lib/bricky/bricks/helper.rb
+++ lib/bricky/bricks/helper.rb
@@ -2,14 +2,15 @@ module Bricky
   module Bricks
     class Helper < Base
       def arguments
-        ["-v #{bricks_path}/helper:/bricks/helper",
-         "-v #{ssh_auth_socket}:/tmp/ssh-auth.socket"]
+        ["-v #{bricks_path}/helper:/bricks/helper"].tap do |args|
+          args.push("-v #{ssh_auth_socket}:/tmp/ssh-auth.socket") unless ssh_auth_socket.nil?
+        end
       end
 
       def environments
-        ["-e BRICKS_LINUX=#{linux}",
-         "-e BRICKS_HOME=/bricks/helper",
-         "-e SSH_AUTH_SOCK=/tmp/ssh-auth.socket"]
+        ["-e BRICKS_LINUX=#{linux}", '-e BRICKS_HOME=/bricks/helper'].tap do |envs|
+          envs.push('-e SSH_AUTH_SOCK=/tmp/ssh-auth.socket') unless ssh_auth_socket.nil?
+        end
       end
 
       private
