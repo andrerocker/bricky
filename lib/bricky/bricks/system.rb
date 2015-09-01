118fc3751dc359d395c068c9d54b78c8e97a90d6
andrerocker <andre.souza@gmail.com>
2015-08-31 21:47:28 -0300

    debian: add 'us' 'uc' to the build command \o/

--- lib/bricky/bricks/system.rb
+++ lib/bricky/bricks/system.rb
@@ -1,16 +1,6 @@
 module Bricky
   module Bricks
     class System < Base
-      def arguments
-        scripts_path = "#{bricks_path}/debian"
-
-        %W(-v #{scripts_path}:/bricks/system)
-      end
-
-      def entrypoint
-        "/bricks/system/builder"
-      end
-
       def bootstrap(bootstrap_path)
         if config.fetch('bootstrap', false)
           config.fetch('bootstrap').collect do |command|
