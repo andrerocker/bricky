8bfd94d1d201aedfbd8397367edff1f444bdea21
andrerocker <andre.souza@gmail.com>
2015-09-17 21:46:35 -0300

    builder: use popen2e to execute all commands

--- lib/bricky/executor.rb
+++ lib/bricky/executor.rb
@@ -5,17 +5,25 @@ module Bricky
     extend self
 
     def popen(label, command)
-      Open3.popen2e(command) do |input, output, wait|
-        output.each { |line| logger.normal label.rjust(8), ": #{line.chomp}" } 
-        wait.value.to_i.eql?(0)
-      end 
+      execute(command) do |line|
+        logger.normal label.rjust(8), ": #{line.chomp}"
+      end
     end
 
     def system(command)
-      Kernel.system(command)
+      execute(command) do |line|
+        logger.info line.chomp
+      end
     end
 
     private
+      def execute(command)
+        Open3.popen2e(command) do |input, output, wait|
+          output.each { |line| yield(line) }
+          wait.value.to_i.eql?(0)
+        end
+      end
+
       def logger
         Bricky.logger
       end
